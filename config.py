99a05ae49b7e2aef8a05da7552637fc7ca5582e2
huybk <huy.2705@gmail.com>
2018-05-06 23:57:23 +0700

    Update code

---
+++ config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+pre_link='https://bittrex.com/api/v1.1/public/getmarketsummary?market=usdt-neo'
+current_value=15.976662
+first_price=29.48
+
+
+#Cấu hình twilio
+sdt='088***'
+account = "***"
+token = "***"
+from_twilio = "+16672399025"
+# may_khach = Client(account, token)
+
+#Cấu hình telegram
+api_telegram='***'
+chat_id='***'
---
+++ send_price.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import requests
+# import os
+import config
+import json
+import time
+import telegram_module
+# from twilio.rest import Client
+#
+#def send_sms(list_nguoi_nhan,noi_dung):
+#	# return nguoi_nhan
+#	may_khach = Client(config.account, config.token)
+#	for a in list_nguoi_nhan:
+#		a="+84"+a[1:len(a)]
+#		message = may_khach.messages.create(to='+84888354345', from_=config.from_twilio,body=noi_dung)
+# print(r.content)
+def main():
+	r = requests.get(config.pre_link)
+	data = json.loads(r.content)
+	if data['success'] == True:
+		# print(data['result'][0])
+		newest_price=data['result'][0]['Last']
+		print(newest_price)
+		change=(newest_price - config.first_price)/config.first_price*100
+		print(change)
+		if change > 0:
+			sms='Gia moi = '+str(newest_price)+'\r\n'+'my volume = '+str(config.current_value*newest_price)+'\r\n'+data['result'][0]['MarketName']
+			# send_sms(config.sdt, sms)
+			a=telegram_module.telegram_send_to(config.chat_id,sms,config.api_telegram)
+			print(a)
+			time.sleep(120)
+if __name__ == '__main__':
+	while True:
+		try:
+			main()
+			time.sleep(120)
+		except Exception as value:
+			print(value)
+			pass
+		time.sleep(120)
+# may_khach = Client(account, token)
+	# print(result)
+# print(data['success'])
